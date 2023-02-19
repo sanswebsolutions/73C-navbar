fdab8fd29147099ee01ad447a7bff6b78a705a33
Nikhil Jain <njain923@gmail.com>
2023-02-19 19:48:10 +0530

    update

--- lib/profilebutton/profilebutton.component.d.ts
+++ lib/profilebutton/profilebutton.component.d.ts
@@ -8,6 +8,7 @@ export declare class ProfilebuttonComponent implements OnInit {
     ProfileMenu(): void;
     OrgMenu(): void;
     LangMenu(): void;
+    Logout(): void;
     static ɵfac: i0.ɵɵFactoryDeclaration<ProfilebuttonComponent, never>;
     static ɵcmp: i0.ɵɵComponentDeclaration<ProfilebuttonComponent, "lib-profilebutton", never, {}, {}, never, never>;
 }
