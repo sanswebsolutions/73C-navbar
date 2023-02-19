feb7a7cae35c003fd40b8214f7bb6d5f156ff18a
Nikhil Jain <njain923@gmail.com>
2023-02-19 19:09:19 +0530

    update

--- lib/profilebutton/profilebutton.component.d.ts
+++ lib/profilebutton/profilebutton.component.d.ts
@@ -3,6 +3,9 @@ import * as i0 from "@angular/core";
 export declare class ProfilebuttonComponent implements OnInit {
     constructor();
     ngOnInit(): void;
+    ProfileMenu(): void;
+    OrgMenu(): void;
+    LangMenu(): void;
     static ɵfac: i0.ɵɵFactoryDeclaration<ProfilebuttonComponent, never>;
     static ɵcmp: i0.ɵɵComponentDeclaration<ProfilebuttonComponent, "lib-profilebutton", never, {}, {}, never, never>;
 }
