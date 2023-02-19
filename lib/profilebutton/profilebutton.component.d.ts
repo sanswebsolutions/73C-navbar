dcd4eddfd60d7c51fc3db82db3a43b8f26e6bdb5
Nikhil Jain <njain923@gmail.com>
2023-02-19 19:33:19 +0530

    update

--- lib/profilebutton/profilebutton.component.d.ts
+++ lib/profilebutton/profilebutton.component.d.ts
@@ -1,6 +1,8 @@
 import { OnInit } from '@angular/core';
 import * as i0 from "@angular/core";
 export declare class ProfilebuttonComponent implements OnInit {
+    user: any;
+    shortName: any;
     constructor();
     ngOnInit(): void;
     ProfileMenu(): void;
